00733f08fcabea71b16e8129e5221962961cfd46
51133134 <cze1330@gmail.com>
2023-03-31 09:46:27 +0800

    66666

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': 'cmsimde_site', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': '我是白癡', 'tags': '', 'url': 'About.html'}]};
